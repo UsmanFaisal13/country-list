9485c7ada96cecd6a9b304ac48c95265a6a9ed58
UsmanFaisal13 <130107471+UsmanFaisal13@users.noreply.github.com>
2024-10-10 16:23:00 +0500

    small text change

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (<>
     <main className="flex min-h-screen flex-col items-center gap-10 p-24">
       <h1 className="font-black text-6xl">Country List</h1>
-      <h3>A GIAIC assignment project </h3>
+      <h3>A GI-AIWMD assignment project </h3>
       <Nav />
 
       <footer className='flex flex-col gap-5 justify-center items-center mt-6'>&copy; created by Usman Faisal </footer>
